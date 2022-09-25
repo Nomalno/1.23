b5a7653daaef561d151f16c9a4045097f5dabc83
Горячев Владимир Сергеевич <51385107+Nomalno@users.noreply.github.com>
2022-09-26 00:22:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        double y;
+            Console.WriteLine("Введите значение a");
+            var a=Convert.ToDouble(Console.ReadLine());
+        y = (Math.Pow(a,2)+10)/(Math.Sqrt(Math.Pow(a, 2) + 1));
+        Console.WriteLine(y);
+        Console.ReadKey();
+    }
+    
+}
